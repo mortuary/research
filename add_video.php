632675e04657cee2eea474bc1ce4293f90cc810b
Alex Chuch <a.jahcoozi@gmail.com>
2014-10-14 18:45:34 +0400

    No button style add_video

--- add_video.php
+++ add_video.php
@@ -1,3 +1,12 @@
+<!-- //Add button -->
+<script type="text/javascript">
+$(document).ready(function() {
+$('<a href="#slideshow" style="background: #1b0504 url(/local/templates/villagio/img/ico-video-arrow.png) no-repeat 25px 22px; left: 0px; padding-left: 70px;  font-size: 20px; text-decoration: none; width: 100px;" class="img-plate__desc img-plate__desc_fix-corner img-plate__link slideshow-btn">Слайдшоу</a>').insertAfter(".img-plate__desc_fix-corner");
+});
+</script>
+
 <? //add video in detail page
-if ($arResult["DISPLAY_PROPERTIES"]['video_watch']) 
-echo $arResult['DISPLAY_PROPERTIES']['video_watch']['DISPLAY_VALUE'];?>
+if ($arResult["DISPLAY_PROPERTIES"]['video_watch']):?>
+<div id="slideshow" style="margin-top: 24px;"> 
+<?echo $arResult['DISPLAY_PROPERTIES']['video_watch']['DISPLAY_VALUE']; 
+endif;?>
